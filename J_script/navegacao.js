d860c7ec31c27e6152209bd1031739830c37b4fe
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-03-14 13:51:09 -0300

    Adição de JavaScript para Botão de navegação!
    Permitindo adicionar novos links a lista apenas no JS sem mudar em todas as paginas diretamente!

---
+++ J_script/navegacao.js
@@ -0,0 +1,32 @@
+let navbarContent = `
+  <nav>
+    <div class="navprin">
+      <h1 class="navprin_h1">PÁGINAS</h1>
+      <ol class="navprin_ol">
+        <a href="pag1.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag2.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag3.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag4.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag5.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag6.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag7.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag8.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag9.html" rel="internal"><li>Pag</li></a>
+
+        <a href="pag10.html" rel="internal"><li>Pag</li></a>
+                      
+      </ol>
+    </div>
+  </nav>
+`
+
+// Insere a navbar 
+document.getElementById('navbar').innerHTML = navbarContent;
