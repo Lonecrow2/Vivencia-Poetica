f621f8b7bce73aa0a0b020529a5ab7ab01da0290
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-04-16 16:44:33 -0300

    nova pag + ajuste de numeração das pags e links

--- J_script/navegacao.js
+++ J_script/navegacao.js
@@ -3,26 +3,28 @@ let navbarContent = `
     <div class="navprin">
       <h1 class="navprin_h1">PÁGINAS</h1>
       <ol class="navprin_ol">
-        <a href="pag1.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag2.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag3.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag4.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag5.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag6.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag7.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag8.html" rel="internal"><li>Pag</li></a>
-
-        <a href="pag9.html" rel="internal"><li>Pag</li></a>
-
+        <a href="pag11.html" rel="internal"><li>Pag</li></a>
+        
         <a href="pag10.html" rel="internal"><li>Pag</li></a>
-                      
+        
+        <a href="pag9.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag8.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag7.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag6.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag5.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag4.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag3.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag2.html" rel="internal"><li>Pag</li></a>
+        
+        <a href="pag1.html" rel="internal"><li>Pag</li></a>
+        
       </ol>
     </div>
   </nav>
